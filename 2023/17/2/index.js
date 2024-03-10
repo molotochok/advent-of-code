a8eb11fcb2deff88177e00ee751c320dfd381428
Markiian Karpa <markiian@amazon.com>
2024-03-10 15:33:18 +0100

    Add day 17

---
+++ 2023/17/2/index.js
@@ -0,0 +1,96 @@
+const fs = require("fs");
+
+let input = fs
+  .readFileSync(process.argv[2] + ".txt")
+  .toString('UTF8')
+  .split(/\r?\n/);
+
+const path = (input) => {
+  const visited = getVisited(input);
+
+  const stack = [];
+  stack.push([0, 0, -1, -1, 0, 1, []]);
+
+  let min = Number.MAX_VALUE;
+  while (stack.length > 0) {
+    let [row, col, pRow, pCol, value, d, p] = stack.pop();
+    if (row < 0 || row >= input.length) continue;
+    if (col < 0 || col >= input[0].length) continue;
+
+    value += +input[row][col];
+
+    if (value - input[0][0] > min) continue;
+
+    if (row == input.length - 1 && col == input[0].length - 1) {
+      if (d >= 4) {
+        min = Math.min(min, value - input[0][0]);
+      }
+      continue;
+    }
+
+    const vKey = `${pRow < row ? "d" : pRow > row ? "u" : pCol < col ? "r" : "l"}_${d}`;
+    const v = visited[row][col].get(vKey);
+    if (v && v <= value) continue;
+
+    visited[row][col].set(vKey, value);
+
+    if (pRow == -1) {
+      stack.push([row + 1, col, row, col, value, d + 1]);
+      stack.push([row, col + 1, row, col, value, d + 1]);
+    } else if (d == 10) {
+      if (pRow < row || pRow > row) {
+        stack.push([row, col - 1, row, col, value, 1]);
+        stack.push([row, col + 1, row, col, value, 1]);
+      } else {
+        stack.push([row - 1, col, row, col, value, 1]);
+        stack.push([row + 1, col, row, col, value, 1]);
+      }
+    } else if (d <= 3) {
+      if (pRow < row) {
+        stack.push([row + 1, col, row, col, value, d + 1]);
+      } else if (pRow > row) {
+        stack.push([row - 1, col, row, col, value, d + 1]);
+      } else if (pCol < col) {
+        stack.push([row, col + 1, row, col, value, d + 1]);
+      } else {
+        stack.push([row, col - 1, row, col, value, d + 1]);
+      }
+    } else {
+      if (pRow < row) {
+        stack.push([row + 1, col, row, col, value, d + 1]);
+        stack.push([row, col - 1, row, col, value, 1]);
+        stack.push([row, col + 1, row, col, value, 1]);
+      } else if (pRow > row) {
+        stack.push([row - 1, col, row, col, value, d + 1]);
+        stack.push([row, col - 1, row, col, value, 1]);
+        stack.push([row, col + 1, row, col, value, 1]);
+      } else if (pCol < col) {
+        stack.push([row - 1, col, row, col, value, 1]);
+        stack.push([row + 1, col, row, col, value, 1]);
+        stack.push([row, col + 1, row, col, value, d + 1]);  
+      } else {
+        stack.push([row - 1, col, row, col, value, 1]);
+        stack.push([row + 1, col, row, col, value, 1]);
+        stack.push([row, col - 1, row, col, value, d + 1]);
+      }
+    }
+  }
+
+  return min;
+}
+
+function getVisited(input) {
+  const v = [];
+  for (let i = 0; i < input.length; i++) {
+    const arr = [];
+    for (let j = 0; j < input[0].length; j++) {
+      arr.push(new Map());
+    } 
+    v.push(arr);
+  }
+  return v;
+};
+
+const startTime = performance.now()
+const res = path(input);
+console.log(`Result: ${res}. Took ${(performance.now() - startTime) / 1000} seconds`);
